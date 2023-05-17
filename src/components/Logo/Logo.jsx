922e1f8a0637509436d130fbe2f3f061018c946f
sshivahh <russelshivah16@gmail.com>
2023-05-17 11:31:28 +0700

    edit kecil kecilan

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,11 +4,13 @@ import styles from './Logo.css'
 function Logo(){
     const navigate = useNavigate();
 
-    return <div className="logoDiv">
-        <button onClick={()=>navigate('/')}>
-            <img className="logoImg" src="src\assets\images\logotesdoang.png"></img>
-        </button>
-    </div>
+    return (
+        <div className="logoDiv ">
+            <button onClick={()=>navigate('/')}>
+                <img className="logoImg" src="src\assets\images\logotesdoang.png"></img>
+            </button>
+        </div>
+    )
 }
 
 export default Logo;
