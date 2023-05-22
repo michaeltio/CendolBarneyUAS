8f48bc767049583bf715b1f39ad4f6c86405138c
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-05-22 18:37:48 +0700

    Benerin Bug Quoty

--- src/pages/Quoty.jsx
+++ src/pages/Quoty.jsx
@@ -5,17 +5,17 @@ export default function Quoty() {
   const category = '';
   const apiKey = 'qdg4xdeAsooYDE538jETSQ==gSkhZ93GlgbeHH9Z';
   const [quote, setQuote] = useState('');
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const [isFirstRender, setFirstRender] = useState(true);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-        headers: { 'X-Api-Key': apiKey },
-        contentType: 'application/json',
-      });
+      const response = await axios.get(
+        `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+        {
+          headers: { 'X-Api-Key': apiKey },
+          contentType: 'application/json',
+        }
+      );
       if (response.data.length > 0) {
         setQuote(response.data[0]);
         console.log(response.data);
@@ -24,6 +24,14 @@ export default function Quoty() {
       console.error('Error:', error.response.data);
     }
   };
+
+  useEffect(() => {
+    if (!isFirstRender) {
+      fetchData();
+    } else {
+      setFirstRender(false);
+    }
+  }, [isFirstRender]);
 
   const handleNewQuote = () => {
     fetchData();
@@ -35,9 +43,14 @@ export default function Quoty() {
       <h1>{quote.category}</h1>
       <div className='sm:w-1/3 mx-auto text-3xl mt-10'>
         <h1>&ldquo;{quote.quote}&ldquo;</h1>
-        <h1 className="text-xl font-bold">&#8212; {quote.author}</h1>
+        <h1 className='text-xl font-bold'>&#8212; {quote.author}</h1>
       </div>
-      <button className='absolute left-1/2 bottom-0 mb-5 -translate-x-1/2' onClick={handleNewQuote}>New Quote</button>
+      <button
+        className='absolute left-1/2 bottom-0 mb-5 -translate-x-1/2'
+        onClick={handleNewQuote}
+      >
+        New Quote
+      </button>
     </div>
   );
 }
