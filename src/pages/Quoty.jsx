df32f2546de491b6ba823c53640416ca306f21dc
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-05-22 19:09:29 +0700

    benerin design Quoty

--- src/pages/Quoty.jsx
+++ src/pages/Quoty.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import "../components/QuotyButton/QuotyButton.css"
 
 export default function Quoty() {
   const category = '';
@@ -39,18 +40,20 @@ export default function Quoty() {
 
   return (
     <div className='text-center mt-20'>
-      <h1 className='text-6xl'>Quoty</h1>
-      <h1>{quote.category}</h1>
-      <div className='sm:w-1/3 mx-auto text-3xl mt-10'>
-        <h1>&ldquo;{quote.quote}&ldquo;</h1>
-        <h1 className='text-xl font-bold'>&#8212; {quote.author}</h1>
+      <div className='m-5'>
+        <h1 className='text-6xl'>Quoty</h1>
+        <h1>{quote.category}</h1>
+        <div className='sm:w-1/3 mx-auto text-3xl mt-10'>
+          <h1>&ldquo;{quote.quote}&ldquo;</h1>
+          <h1 className='text-xl font-bold mt-5'>&#8212; {quote.author}</h1>
+        </div>
+        <button
+          className='mt-20 mb-10 button type1'
+          onClick={handleNewQuote}
+        >
+          <span className="btn-txt">Next Quote</span>
+        </button>
       </div>
-      <button
-        className='absolute left-1/2 bottom-0 mb-5 -translate-x-1/2'
-        onClick={handleNewQuote}
-      >
-        New Quote
-      </button>
     </div>
   );
 }
