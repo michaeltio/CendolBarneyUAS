597fbb9129d5f76b35987b10e2ec28cc915badba
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-05-23 18:54:47 +0700

    Bikin Would You Rather

--- src/pages/WouldYouRather.jsx
+++ src/pages/WouldYouRather.jsx
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 export default function WouldYouRather() {
     const [data, setData] = useState(null);
+    const [question, setQuestion] = useState({
+        option1 : "",
+        option2 : ""
+    });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,13 +25,24 @@ export default function WouldYouRather() {
         };
 
         fetchData();
+
+        const processQuestion = () => {
+            const option1 = data?.substring(data.indexOf("rather") +7 , data.indexOf("or")-1);
+            const option2 = data?.substring(data.indexOf("or") + 3, data.indexOf("?"));
+            console.log(option1);
+            console.log(option2);
+            setQuestion({option1 : option1, option2 : option2})
+        }
+        processQuestion();
     }, []);
 
     return (
         <div>
             {data ? (
                 <div>
-                <h1>{data[0].question}</h1>
+                    <h1>Would You Rather?</h1>
+                    <h1>{question.option1}</h1>
+                    <h1>{question.option2}</h1>
                 </div>
             ) : (
                 <p>Loading...</p>
