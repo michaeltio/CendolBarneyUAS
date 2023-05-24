30f26baa1561c5c734a7e303d4fe334ab621b995
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-05-24 10:31:39 +0700

    Benerin Card Cardnya

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,53 +14,54 @@ export default function Home(){
                     img = {Card}
                 />
                 <CardButton
-                    route="/magic8ball" 
-                    img = ""
-                    name = "2. magic 8 ball"
-                />  
+                    route="/primeornot"
+                    img = "" 
+                    name = "2. Prime Or Not"
+                />
                 <CardButton
                     route="/astronomylibrary"
                     img = "" 
-                    name = "3. since birth"
+                    name = "3. Astronomy Library"
                 />
                 <CardButton
-                    route="/primeornot"
-                    img = "" 
-                    name = "4. "
-                />
-                <CardButton
-                    route="/"
+                    route="/magic8ball" 
                     img = ""
-                    name = "5. " 
+                    name = "4. magic 8 ball"
+                />  
+                <CardButton
+                   route="/sincebirth"
+                   img = "" 
+                    name = "5. Since Birth" 
                 />
                 <CardButton
                     route="/quoty"
                     img = "" 
+                    name = "6. Quoty"
                 />
                 <CardButton
-                    route="/sincebirth"
-                    img = "" 
-                    name = "6. "
-                />
-                <CardButton
-                    route="/"
+                    route="/wouldyourather"
                     img = ""
-                    name = "7. " 
+                    name = "7. Would You Rather" 
+                />
+                <CardButton
+                    route="/dogsfromallovertheworld"
+                    img = "" 
+                    name = "8. Dogs From All Over The World"
                 />
                 <CardButton
                     route="/"
                     img = "" 
-                    name = "8. "
+                    name = "9. (Belum Ada Game)"
                 />
                 <CardButton
                     route="/"
                     img = "" 
-                    name = "9. "
+                    name = "10. (Belum Ada Game)"
                 />
                 <CardButton
-                    route="/"
+                    route="/Credit"
                     img = "" 
-                    name = "10. "
+                    name = "11. Credit"
                 />
             </div>
         </div>
