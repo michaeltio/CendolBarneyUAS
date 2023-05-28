294ae98eedb4622bae829262c5ee0ac3edb412f6
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-05-28 18:23:22 +0700

    Benerin Card Button

--- src/components/CardButton/CardButton.jsx
+++ src/components/CardButton/CardButton.jsx
@@ -4,14 +4,16 @@ import './CardButton.css'
 function CardButton(props){
     const navigate = useNavigate();
 
-    return <div className="card m-3">
-        <button className="cardButton" onClick={()=>navigate(props.route)}>
-            <div className="imageWrapper">
-                <p>{props.name}</p>
-                <img className="buttonImage" src={props.img} />
-            </div>
-        </button>
-    </div>
+    return (
+        <div className="card m-3">
+            <button className="cardButton" onClick={()=>navigate(props.route)}>
+                <div className="imageWrapper">
+                    <p>{props.name}</p>
+                    <img src={props.img} />
+                </div>
+            </button>
+        </div>
+    )
 }
 
 export default CardButton;
